a53135c8d56ca7655f1760008d5133b4fd564a59
Thanos Dosis <thanosdosis7@gmail.com>
2022-11-23 10:12:35 +0200

    Update v1.0

--- Calculator.py
+++ Calculator.py
@@ -9,17 +9,21 @@ def sub_function():
 def multi_function():
     e5.insert(0, str(int(e1.get()) * int(e2.get())))
 
+def div_function():
+    e6.insert(0, str(int(e1.get()) // int(e2.get())))
+
 def clear_entries():
     e1.delete(0,10)
     e2.delete(0,10)
     e3.delete(0,10)
     e4.delete(0,10)
     e5.delete(0,10)
+    e6.delete(0,10)
     
 
 root=Tk()
 
-l1=Label(root, text='Calculator v0.3')
+l1=Label(root, text='Calculator v1.0')
 l1.grid(row=0, sticky=W)
 l2=Label(root, text='1ος αριθμός:')
 l2.grid(row=1, sticky=W)
@@ -41,6 +45,10 @@ b3=Button(root, text=' * ', fg='navy', command=multi_function)
 b3.grid(row=5, sticky=W)
 e5=Entry(root)
 e5.grid(row=5, column=1)
+b4=Button(root, text=' / ', fg='navy', command=div_function)
+b4.grid(row=6, sticky=W)
+e6=Entry(root)
+e6.grid(row=6, column=1)
 b5=Button(root, text='Καθαρισμός', fg='navy', command=clear_entries)
 b5.grid(row=7, sticky=W)
 
